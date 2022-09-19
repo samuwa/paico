55ef03b40a226f6e5255638d9da88afbc27a9a03
samuwa <69830704+samuwa@users.noreply.github.com>
2022-09-19 11:16:20 -0500

    Update paico_streamlit.py

--- paico_streamlit.py
+++ paico_streamlit.py
@@ -65,7 +65,7 @@ cierre = col4.date_input("Fecha de cierre")
 if len(indus) == 0:
     pass
 elif len(indus) == 1:
-    total_industria_query = run_query(f"select clientes.industria, sum(ventas.total) from clientes join ventas on ventas.cliente = clientes.clienteid where clientes.industria in {"("+indus[0]+")"} group by clientes.industria")
+    total_industria_query = run_query(f"select clientes.industria, sum(ventas.total) from clientes join ventas on ventas.cliente = clientes.clienteid where clientes.industria in ({indus[0]}) group by clientes.industria")
     total_industria_df = pd.DataFrame(total_industria_query, columns=["Industria", "Total"])
     st.write(total_industria_df)                                  
     
